d50248372cbb8d5015fca9296acd5856de54f2c3
Andrew <117526699+apratajr@users.noreply.github.com>
2023-11-11 13:54:02 -0500

    Fix concurrency (singlethread/dualthread working)

--- Project_4/main.cpp
+++ Project_4/main.cpp
@@ -14,32 +14,42 @@
 #include <algorithm>
 #include <map>
 #include <iomanip>
+#include <thread>
+#include <vector>
+#include <cmath>
+#include <mutex>
 
 // Class for the encoding translation dictionary
 class EncoderDictionary {
 private:
     std::map<std::string, int> dictionary;
     int nextValue;
+    std::mutex dictionaryMutex; // Mutex for contruction synchronization
 public:
     // Constructor
     EncoderDictionary() : nextValue(0) {}
 
     // Function to add a key
     int addKey(const std::string& key) {
+        std::lock_guard<std::mutex> lock(dictionaryMutex); // Lock the mutex to prevent thread collision
+        //std::cout << "Structure locked" << std::endl;
         // Iterator to the KVP
         auto it = dictionary.find(key);
         // Key already exists, return its value
         if (it != dictionary.end()) {
+            //std::cout << "Exiting scope, releasing lock" << std::endl;
             return it->second;
         // Key doesn't exist, add it with the next integer encoding
         } else {
             dictionary[key] = nextValue;
+            //std::cout << "Exiting scope, releasing lock" << std::endl;
             return nextValue++;
         }
     }
 
     // Function to retrieve a key encoding value
     int getEncoding(const std::string& key) {
+        std::lock_guard<std::mutex> lock(dictionaryMutex); // Lock the mutex (this may be multithreaded eventually)
         return dictionary.find(key)->second;
     }
 
@@ -52,10 +62,38 @@ public:
     size_t size() const {
         return dictionary.size();
     }
+
+    // Function to access the dictionary mutex (not needed just yet.)
+    std::mutex& getMutex() {
+        return dictionaryMutex;
+    }
 };
+
+void asyncProcessor(const std::string& filepath_in, size_t start, size_t end, EncoderDictionary& d) {
+    // Open & Verify
+    std::ifstream file_in_async(filepath_in);
+    if (!file_in_async.is_open()) {
+        std::cerr << "Error: Could not open the input file " << filepath_in << '\n';
+        return;
+    }
+
+    // Seek to the beginning of the desired line
+    for (size_t i = 0; i < start; ++i) {
+        file_in_async.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    // Process lines from start to end of thread region
+    std::string line;
+    size_t current_position = start;
+    while (current_position < end && std::getline(file_in_async, line)) {
+        d.addKey(line); // Mutex critical region. Must be multithread protected (see class)
+        current_position++;
+    }
+}
 
 // Form the dictionary itself, with keys as the original data and values as the encodings
 void createDictionary(const std::string& filepath_in, const std::string& dictpath_out, EncoderDictionary& d) {
+    // Open and verify streams
     std::ofstream dict_out(dictpath_out);
     if (!dict_out.is_open()) {
         std::cerr << "Error: Could not open the dictionary file " << dictpath_out << '\n';
@@ -66,11 +104,31 @@ void createDictionary(const std::string& filepath_in, const std::string& dictpat
         std::cerr << "Error: Could not open the input file " << filepath_in << '\n';
         return;
     }
+
+    // Prepare for multithreading split
+    const size_t num_threads = 2; //std::thread::hardware_concurrency(); // Get the number of available hardware threads
+    size_t line_count = 0;
     std::string line;
-    dict_out << std::hex;
     while (std::getline(file_in, line)) {
-        d.addKey(line);
+        ++line_count;
     }
+    const size_t lines_per_thread = floor(line_count / num_threads);
+    std::vector<std::thread> threads;
+
+    // Divide work over thread count and asynchronously build dictionary data structure
+    for (size_t i = 0; i < num_threads; ++i) {
+        size_t start = i * lines_per_thread;
+        size_t end = (i == num_threads-1) ? line_count-1 : start + lines_per_thread;
+        threads.emplace_back([&, start, end](){ asyncProcessor(filepath_in, start, end, d); });
+    }
+
+    // Synchronize all threads
+    for (size_t i = 0; i < num_threads; ++i) {
+        threads[i].join();
+    }
+
+    // Contruct dictionary text file (singlethreaded, otherwise mutex issues)
+    dict_out << std::hex; // Testing of encoded sizes and such
     for (const auto& entry : d.getDictionary()) {
         dict_out << entry.first << ":" << entry.second << '\n';
     }
